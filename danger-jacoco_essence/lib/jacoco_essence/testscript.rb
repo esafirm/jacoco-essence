319f5ffa274b9132c7463e5e14ec32ab05ebc45d
Esa Firman <esafirm21@gmail.com>
2019-10-20 15:36:02 +0700

    Fix something

--- danger-jacoco_essence/lib/jacoco_essence/testscript.rb
+++ danger-jacoco_essence/lib/jacoco_essence/testscript.rb
@@ -2,9 +2,16 @@
 
 jarFile = "#{File.dirname(__FILE__)}/jacoco-essence-1.0-SNAPSHOT.jar"
 jacocoFile = "#{File.dirname(__FILE__)}/../../spec/fixtures/jacoco.xml"
+min = "10"
 
-puts "'java -jar #{jarFile} -g --input=#{jacocoFile}'"
+puts "'java -jar #{jarFile} -g --input=#{jacocoFile}' --min=#{min}"
 
 markdown_report = `java -jar #{jarFile} -g --input="#{jacocoFile}"`
 
+code = system("java -jar #{jarFile} -g --input=\"#{jacocoFile}\"")
+
+puts "Code: #{code}"
+
+puts $?.exitstatus 
+puts "Exit status #{$?.exitstatus}"
 puts markdown_report
